f7165b499a34efb26d743ca584a428bd9dfb2691
devjkuba <jindrich.kuba@cyberdev.cz>
2025-05-06 22:30:07 +0200

    add role

--- server/src/registerUser.ts
+++ server/src/registerUser.ts
@@ -8,9 +8,9 @@ export const registerUserHandler = async (req: Request, res: Response): Promise<
     return;
   }
 
-  const { firstName, lastName, email, password, confirmPassword, organizationId } = req.body;
+  const { firstName, lastName, email, password, role, confirmPassword, organizationId } = req.body;
 
-  if (!firstName || !lastName || !email || !password || !confirmPassword || !organizationId) {
+  if (!firstName || !lastName || !email || !password || !confirmPassword || !organizationId || !role) {
     res.status(400).json({ message: 'Missing required fields' });
     return;
   }
@@ -35,7 +35,7 @@ export const registerUserHandler = async (req: Request, res: Response): Promise<
         lastName,
         email,
         password: hashedPassword,
-        role: 'USER',
+        role,
         isActive: true,
         organizationId, 
       },
