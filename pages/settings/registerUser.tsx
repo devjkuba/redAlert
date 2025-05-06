0ac8a3d44070d041aafbb0be001563657bce7938
devjkuba <jindrich.kuba@cyberdev.cz>
2025-05-06 21:52:36 +0200

    register users

--- pages/settings/registerUser.tsx
+++ pages/settings/registerUser.tsx
@@ -1,15 +1,22 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { toast } from "sonner";
 import { Toaster } from "@/components/ui/sonner";
 import Navbar from "@/components/Navbar";
 import useAuthToken from "@/hooks/useAuthToken";
 import useDemo from "@/hooks/useDemo";
 import useUser from "@/hooks/useUser";
+import router from "next/router";
 
 const registerUserSchema = z
   .object({
@@ -36,38 +43,41 @@ const registerUserSchema = z
     path: ["confirmPassword"],
   });
 
-  interface RegisterUserFormData {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-    role: string;
-  }
+interface RegisterUserFormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  role: string;
+}
 
-  export default function RegisterUser() {
-    const { data: user } = useUser();
-    const organizationId = user?.organizationId;
-  
-    const token = useAuthToken();
-    const { isDemoActive } = useDemo();
-  
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm({
-      resolver: zodResolver(registerUserSchema),
-    });
+export default function RegisterUser() {
+  const { data: user } = useUser();
+  const organizationId = user?.organizationId;
 
-    const onSubmit = async (data: RegisterUserFormData): Promise<void> => {
-      // Kontrola, zda je organizationId k dispozici
-      if (!organizationId) {
-        toast.error("Organizace není dostupná.");
-        return;
-      }
-  
-      const res: Response = await fetch(`${process.env.NEXT_PUBLIC_API}api/register-user`, {
+  const token = useAuthToken();
+  const { isDemoActive } = useDemo();
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registerUserSchema),
+  });
+
+  const onSubmit = async (data: RegisterUserFormData): Promise<void> => {
+    // Kontrola, zda je organizationId k dispozici
+    if (!organizationId) {
+      toast.error("Organizace není dostupná.");
+      return;
+    }
+
+    const res: Response = await fetch(
+      `${process.env.NEXT_PUBLIC_API}api/register-user`,
+      {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -78,90 +88,116 @@ const registerUserSchema = z
           lastName: data.lastName,
           email: data.email,
           password: data.password,
+          confirmPassword: data.confirmPassword,
           role: data.role,
           organizationId,
         }),
-      });
-  
-      if (res.ok) {
-        toast.success("Uživatel byl úspěšně zaregistrován.");
-      } else {
-        toast.error("Chyba při registraci uživatele.");
       }
-    };
-  
-    return (
-      <div className="flex min-h-screen !pt-safe !px-safe pb-safe border-0 mx-auto max-w-4xl w-full">
-        <main className="relative overflow-hidden flex flex-col flex-grow items-center justify-start">
-          {isDemoActive && (
-            <div className="absolute bg-[#982121] text-white font-sm w-full text-center font-bold text-sm">
-              DEMO
-            </div>
-          )}
-          <div className="absolute top-4 left-1/2 -translate-x-1/2 text-center">
-            <img src="/logo.png" alt="Logo" className="w-48 h-auto mb-2" />
-          </div>
-          <Navbar />
-          <Toaster position="bottom-center" />
-          <div className="w-full max-w-4xl px-4 pb-7">
-            <div className="max-w-md mx-auto mt-10">
-              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-                <Input
-                  {...register("firstName")}
-                  placeholder="Jméno"
-                  className={errors.firstName ? "border-red-500" : ""}
-                />
-                {errors.firstName && <span className="text-red-500">{errors.firstName.message}</span>}
-  
-                <Input
-                  {...register("lastName")}
-                  placeholder="Příjmení"
-                  className={errors.lastName ? "border-red-500" : ""}
-                />
-                {errors.lastName && <span className="text-red-500">{errors.lastName.message}</span>}
-  
-                <Input
-                  {...register("email")}
-                  placeholder="Email"
-                  type="email"
-                  className={errors.email ? "border-red-500" : ""}
-                />
-                {errors.email && <span className="text-red-500">{errors.email.message}</span>}
-  
-                <Input
-                  {...register("password")}
-                  placeholder="Heslo"
-                  type="password"
-                  className={errors.password ? "border-red-500" : ""}
-                />
-                {errors.password && <span className="text-red-500">{errors.password.message}</span>}
-  
-                <Input
-                  {...register("confirmPassword")}
-                  placeholder="Potvrďte heslo"
-                  type="password"
-                  className={errors.confirmPassword ? "border-red-500" : ""}
-                />
-                {errors.confirmPassword && (
-                  <span className="text-red-500">{errors.confirmPassword.message}</span>
-                )}
-  
-                <Select {...register("role")}>
-                  <SelectTrigger className={`w-full ${errors.role ? "border-red-500" : ""}`}>
-                    <SelectValue placeholder="Vyberte roli" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="ADMIN">ADMIN</SelectItem>
-                    <SelectItem value="USER">USER</SelectItem>
-                  </SelectContent>
-                </Select>
-                {errors.role && <span className="text-red-500">{errors.role.message}</span>}
-                <Button type="submit">Registrovat</Button>
-              </form>
-            </div>
-          </div>
-        </main>
-      </div>
     );
-  }
-  
+
+    if (res.ok) {
+      toast.success("Uživatel byl úspěšně zaregistrován.");
+
+      router.push("/settings/users");
+    } else {
+      toast.error("Chyba při registraci uživatele.");
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen !pt-safe !px-safe pb-safe border-0 mx-auto max-w-4xl w-full">
+      <main className="relative overflow-hidden flex flex-col flex-grow items-center justify-start">
+        {isDemoActive && (
+          <div className="absolute bg-[#982121] text-white font-sm w-full text-center font-bold text-sm">
+            DEMO
+          </div>
+        )}
+        <div className="absolute top-4 left-1/2 -translate-x-1/2 text-center">
+          <img src="/logo.png" alt="Logo" className="w-48 h-auto mb-2" />
+        </div>
+        <Navbar />
+        <Toaster position="bottom-center" />
+        <div className="w-full max-w-4xl px-4 pb-7">
+          <div className="max-w-md mx-auto mt-10">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-4"
+            >
+              <Input
+                {...register("firstName")}
+                placeholder="Jméno"
+                className={errors.firstName ? "border-red-500" : ""}
+              />
+              {errors.firstName && (
+                <span className="text-red-500">{errors.firstName.message}</span>
+              )}
+
+              <Input
+                {...register("lastName")}
+                placeholder="Příjmení"
+                className={errors.lastName ? "border-red-500" : ""}
+              />
+              {errors.lastName && (
+                <span className="text-red-500">{errors.lastName.message}</span>
+              )}
+
+              <Input
+                {...register("email")}
+                placeholder="Email"
+                type="email"
+                className={errors.email ? "border-red-500" : ""}
+              />
+              {errors.email && (
+                <span className="text-red-500">{errors.email.message}</span>
+              )}
+              <Input
+                {...register("password")}
+                placeholder="Heslo"
+                type="password"
+                className={errors.password ? "border-red-500" : ""}
+              />
+              {errors.password && (
+                <span className="text-red-500">{errors.password.message}</span>
+              )}
+              <Input
+                {...register("confirmPassword")}
+                placeholder="Potvrďte heslo"
+                type="password"
+                className={errors.confirmPassword ? "border-red-500" : ""}
+              />
+              {errors.confirmPassword && (
+                <span className="text-red-500">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+              <Controller
+                name="role"
+                control={control}
+                rules={{ required: "Role je povinná" }}
+                render={({ field }) => (
+                  <Select value={field.value} onValueChange={field.onChange}>
+                    <SelectTrigger
+                      className={`w-full ${
+                        errors.role ? "border-red-500" : ""
+                      }`}
+                    >
+                      <SelectValue placeholder="Vyberte roli" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="ADMIN">ADMIN</SelectItem>
+                      <SelectItem value="USER">USER</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              {errors.role && (
+                <span className="text-red-500">{errors.role.message}</span>
+              )}
+              <Button type="submit">Registrovat</Button>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
