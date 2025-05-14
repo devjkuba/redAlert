ecfa5c6d56549e911ccdbf920e2fa81a0d0cb31d
devjkuba <jindrich.kuba@cyberdev.cz>
2025-05-14 23:10:32 +0200

    update

--- components/Push.ts
+++ components/Push.ts
@@ -1,4 +1,4 @@
-const PUBLIC_VAPID_KEY = process.env.NEXT_PRIVATE_KEY ?? '';
+const PUBLIC_VAPID_KEY = urlBase64ToUint8Array(process.env.NEXT_PUBLIC_KEY);
 
 const subscribeToPush = async (userId: number, token: string) => {
     const registration = await navigator.serviceWorker.ready;
@@ -21,5 +21,24 @@ const subscribeToPush = async (userId: number, token: string) => {
       credentials: 'include',
     });
   };
+
+  export function urlBase64ToUint8Array(base64String?: string): Uint8Array {
+    if (!base64String) {
+      throw new Error("Base64 string is required");
+    }
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+      .replace(/-/g, '+')
+      .replace(/_/g, '/');
+  
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+  
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+  
+    return outputArray;
+  }
 
   export default subscribeToPush;
