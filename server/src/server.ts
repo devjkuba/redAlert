e2bce44cc869f2715aacaa5441ea761b2ac8e2d0
devjkuba <jindrich.kuba@cyberdev.cz>
2025-05-24 17:11:58 +0200

    update socket io

--- server/src/server.ts
+++ server/src/server.ts
@@ -42,6 +42,7 @@ const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000", "https://localhost", "capacitor://localhost", "https://redalert.cyberdev.cz"],
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
