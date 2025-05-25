9c6caca7568d05d1ddfb4b54f27afa169227e37c
devjkuba <jindrich.kuba@cyberdev.cz>
2025-05-25 11:21:03 +0200

    server update

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,7 +29,7 @@ app.use(cors({
     "https://redalert.cyberdev.cz",
   ],
   credentials: true,
-  methods: ["GET", "POST", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.options('*', cors());
