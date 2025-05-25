145fb3efcf65f6638176b01b27f56c232331372f
devjkuba <jindrich.kuba@cyberdev.cz>
2025-05-25 10:13:30 +0200

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 text-sm py-1 z-50 px-4 mx-auto max-w-4xl flex justify-center">
+    <footer className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 text-[8px] pb-1 z-50 pr-[calc(1rem_+_env(safe-area-inset-right))] pl-[calc(1rem_+_env(safe-area-inset-left))] mx-auto max-w-4xl flex justify-center">
       <div className="flex w-full justify-between items-center text-muted-foreground">
         <span>© {year} CyberDev s.r.o.</span>
         <a
