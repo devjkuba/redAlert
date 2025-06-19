0722a510d5e94f362497850bd184fd3d8d6598ff
devjkuba <jindrich.kuba@cyberdev.cz>
2025-06-19 20:51:52 +0200

    smazani cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,9 @@ self.addEventListener("activate", (event) => {
 
   event.waitUntil(
     (async () => {
+      const cacheNames = await caches.keys();
+      await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+
       const clientsList = await self.clients.matchAll({ type: "window" });
       clientsList.forEach((client) => {
         client.postMessage({ type: "sw-activated" });
