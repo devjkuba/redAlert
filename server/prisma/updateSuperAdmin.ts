063f26c9dfab49988a99aa449b1b7ad4cf16b8b1
devjkuba <jindrich.kuba@cyberdev.cz>
2025-08-31 23:13:03 +0200

    super admin

---
+++ server/prisma/updateSuperAdmin.ts
@@ -0,0 +1,16 @@
+import { PrismaClient, Role } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const updatedUser = await prisma.user.update({
+    where: { email: "jindrich.kuba@cyberdev.cz" },
+    data: { role: Role.SUPERADMIN },
+  });
+
+  console.log("Uživatel aktualizován:", updatedUser);
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
